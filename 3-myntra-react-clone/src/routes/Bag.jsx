8bce7e93a36eb5d5dabbe0190f87dcaa7fb254c6
Saransh <saranshsharma2103@gmail.com>
2024-07-01 16:56:15 +0530

    First Commit

---
+++ 3-myntra-react-clone/src/routes/Bag.jsx
@@ -0,0 +1,25 @@
+import { useSelector } from "react-redux";
+import BagSummary from "../components/BagSummary";
+import Bagitems from "../components/Bagitems";
+
+const Bag = () => {
+  const bagItems = useSelector((state) => state.bag);
+  const items = useSelector((state) => state.items);
+  const finalItems = items.filter((item) => {
+    const itemIndex = bagItems.indexOf(item.id);
+    return itemIndex >= 0;
+  });
+  return (
+    <main>
+      <div className="bag-page">
+        <div className="bag-items-container">
+          {finalItems.map((item) => (
+            <Bagitems item={item} />
+          ))}
+        </div>
+        <BagSummary />
+      </div>
+    </main>
+  );
+};
+export default Bag;
